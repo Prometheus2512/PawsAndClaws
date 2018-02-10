82a193e2998536497f648e892330d6dd2eca570f
Unknown <mohamedridha.mejri@esprit.tn>
2018-02-10 02:46:34 +0100

    Back/Usertable
    displaying all users in the dashboard

--- src/MainBundle/Controller/AdminController.php
+++ src/MainBundle/Controller/AdminController.php
@@ -10,8 +10,13 @@ class AdminController extends Controller
     {
         return $this->render('MainBundle:admin:index.html.twig');
     }
-    public function tablesAction()
-    {
-        return $this->render('MainBundle:admin:tables.html.twig');
+
+    public function tablesAction() {
+        //access user manager services
+
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+
+        return $this->render('MainBundle:admin:tables.html.twig', array('users' =>   $users));
     }
 }
