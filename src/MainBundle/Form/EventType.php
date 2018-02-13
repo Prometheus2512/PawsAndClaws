610a527aaed1c718caeaf421dc24c4adfdd854a8
Prometheus2512 <mohamedridha.mejri@esprit.tn>
2018-02-13 02:55:20 +0100

    Event Crud
    Event crud added , only the owner of the event and the admin can delete or edit an event

---
+++ src/MainBundle/Form/EventType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')
+                ->add('description')
+                ->add('beginningDate')
+                ->add('endingDate')
+                ->add('address')
+                ->add('posx')
+                ->add('posy')
+                ->add('capacity')
+                ->add('cost')
+                ;
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MainBundle\Entity\Event'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mainbundle_event';
+    }
+
+
+}
