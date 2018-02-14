96ee68ec95ac23833ccc886421469aaa20a6a0a0
Prometheus2512 <mohamedridha.mejri@esprit.tn>
2018-02-14 23:47:15 +0100

    the admin can delete an event

--- src/MainBundle/Controller/AdminController.php
+++ src/MainBundle/Controller/AdminController.php
@@ -29,4 +29,22 @@ class AdminController extends Controller
         return $this->render('MainBundle:admin:eventstable.html.twig', array(
             'events' => $events,
         ));}
+
+    public function eventsdeleteAction($id) {
+        //access user manager services
+
+        $em = $this->getDoctrine()->getManager();
+
+        $event = $em->getRepository('MainBundle:Event')->find($id);
+        $em->remove($event);
+        $em->flush()
+        ;
+        $events = $em->getRepository('MainBundle:Event')->findAll(array('beginningDate' => 'DESC'));
+
+        return $this->render('MainBundle:admin:eventstable.html.twig', array(
+            'events' => $events,
+        ));
+    }
+
+
 }
